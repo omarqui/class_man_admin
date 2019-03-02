2f6c59aeaafe32285a43eae82736366edb3390ce
Omarqui Aybar <omarqui1996@gmail.com>
2019-03-02 12:03:19 -0400

    inicio de funcionamiento de menu lateral

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,10 +10,14 @@ import MenuLateral from '../MenuLateral';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faIgloo, faPenAlt, faBook, faHourglassHalf, 
          faAngleDown, faAngleUp, faPlus, faPencilAlt,
-         faTrash, faSearch, faSave, faTimes } from '@fortawesome/free-solid-svg-icons';
-import MaestroGenerico from '../MaestroGenerico';
+         faTrash, faSearch, faSave, faTimes } from '@fortawesome/free-solid-svg-icons';         
+import MaestroCiclo from '../MaestroCiclo';
 import MaestroProfesor from '../MaestroProfesor';
 import MaestroMateria from '../MaestroMateria';
+
+const MAESTRO_MATERIA = 1;
+const MAESTRO_PROFESOR = 2;
+const MAESTRO_CICLO = 3;
 
 library.add(faIgloo, faPenAlt, faBook, faHourglassHalf, 
             faAngleDown, faAngleUp, faPlus, faPencilAlt,
@@ -23,23 +27,50 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      open: true
+      open: true,
+      formActivo:  1
     };
   }
+
+  cambiarForm(form){
+    this.setState({
+      formActivo: form
+    });
+  }
+
   render() {
+    let formActivo;
+
+    switch (this.state.formActivo) {
+      case MAESTRO_MATERIA:
+        formActivo = <MaestroMateria />;
+        break;
+      case MAESTRO_PROFESOR:
+        formActivo = <MaestroProfesor />;
+        break;
+      case MAESTRO_CICLO:
+        formActivo = <MaestroCiclo />;
+        break;
+    
+      default:
+        break;
+    }
+
     return (
       <div>
-      <MenuTopBar />
-      <Container className="container-fluid">
-      <Row>
-          <Col xs={4} md = {3} lg={2}>            
-            <MenuLateral />
-          </Col>
-          <Col >
-            <MaestroMateria />     
-          </Col>          
-      </Row>  
-      </Container>    
+        <MenuTopBar />
+        <Container className="container-fluid">
+          <Row>
+              <Col xs={4} md = {3} lg={2}>            
+                <MenuLateral clickHandler = { this.cambiarForm }/>
+              </Col>
+              <Col >
+                {
+                  formActivo
+                }                
+              </Col>          
+          </Row>  
+        </Container>    
       </div>
     );
   }
